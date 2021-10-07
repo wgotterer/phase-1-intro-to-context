a3ddae1459098d16ad156d912de16ebf1e53ac18
William Gotterer <william.gotterer@gmail.com>
2021-10-07 09:33:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+
+function createEmployeeRecord(array){
+    return {firstName: array[0], familyName:array[1], 
+    title:array[2], payPerHour:array[3], timeInEvents:[], timeOutEvents:[]}
+    
+}
+
+function createEmployeeRecords(arrOfArrs){
+    return  arrOfArrs.map(createEmployeeRecord)
+    }
+
+function createTimeInEvent(bpRecord, timeString){
+
+   bpRecord.timeInEvents.push({type:"TimeIn", date:timeString.split(" ")[0], hour:timeString.split(" ")[1] })
+
+
+   return bpRecord
+
+///do i need to seperate to line 14 or can I return with push?
+}
+
+
+
+
